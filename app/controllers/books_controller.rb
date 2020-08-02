940e8a8a138ba4b2b786dc42cc80ea603f985d10
vagrant <k.taki415@gmail.com>
2020-08-02 10:22:09 +0000

    [Fix] 6-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,16 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
       render 'index'
     end
@@ -42,7 +45,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
