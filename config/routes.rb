610fcf8d088f5fa14dd0ba74b89f4a70f1d86ee3
vagrant <k.taki415@gmail.com>
2020-08-05 10:33:51 +0000

    [Update] 6-4

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 
 
 end
